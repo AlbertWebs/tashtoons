21ac7ac88c122fc213a6b58eaad97aaf38082fd6
AlbertWebs <albertmuhatia@gmail.com>
2021-07-13 12:17:47 +0300

    VimeoFix

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -42,7 +42,7 @@
             <div class="col-md-2 col-sm-3 col-xs-6 sb1-1">
                 <a href="#" class="btn-close-menu"><i class="fa fa-times" aria-hidden="true"></i></a>
                 <a href="#" class="atab-menu"><i class="fa fa-bars tab-menu" aria-hidden="true"></i></a>
-                <a href="{{url('/')}}/admin/home" class="logo"><img src="{{url('/')}}/uploads/logo/{{$SiteSettings->logo}}" alt="{{$SiteSettings->sitename}}" />
+                <a href="{{url('/')}}/admin/home" class="logo"><img src="{{url('/')}}/uploads/logo/{{$SiteSettings->logo_two}}" alt="{{$SiteSettings->sitename}}" />
                 </a>
             </div>
             <!--== SEARCH ==-->
